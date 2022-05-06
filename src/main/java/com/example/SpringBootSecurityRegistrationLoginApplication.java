9568c7a1466883ea3c3c99ca2f1594f5939ce9ea
Arijit Das <arijit10das2002@gmail.com>
2022-05-06 20:04:35 +0530

    first commit

---
+++ src/main/java/com/example/SpringBootSecurityRegistrationLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityRegistrationLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityRegistrationLoginApplication.class, args);
+	}
+
+}
