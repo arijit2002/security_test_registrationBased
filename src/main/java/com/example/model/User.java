7f2022cce32228caf7c25f12fd3e09fde083e5ce
Arijit Das <arijit10das2002@gmail.com>
2022-05-06 21:30:14 +0530

    Update User.java

--- src/main/java/com/example/model/User.java
+++ src/main/java/com/example/model/User.java
@@ -2,12 +2,15 @@ package com.example.model;
 
 import java.util.Collection;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
@@ -32,7 +35,14 @@ class User {
 	
 	private String password;
 	
-	@ManyToMany(fetch = FetchType.EAGER)
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinTable(
+			name="users_roles",
+			joinColumns = @JoinColumn(
+					name="user_id",referencedColumnName = "id"),
+			inverseJoinColumns = @JoinColumn(
+					name="role_id",referencedColumnName = "id")
+			)
 	private Collection<Role> roles;
 	
 	public User(long id, String firstName, String lastName, String email, String password, Collection<Role> roles) {
